6b43eff15426b4b69154a34a14cdbb2f1dfb4aac
Syed Ahkam <smahkam57@gmail.com>
2022-12-09 04:51:09 +0530

    Set up tailwind with tokyonight color scheme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    );
+    <div className="container mx-16 my-16">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <br />
+      <h2 className="text-blue">Blue</h2>
+      <h2 className="text-green">Green</h2>
+      <h2 className="text-magenta">Magenta</h2>
+      <h2 className="text-yellow">Yellow</h2>
+      <h2 className="text-cyan">Cyan</h2>
+      <h2 className="text-red">Red</h2>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -6,7 +7,21 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      background: "#0f0f14",
+      foreground: "#c0caf5",
+      selection: "#7aa2f7", // same as blue
+      comment: "#5b6474",
+      green: "#9ece6a",
+      blue: "#7aa2f7",
+      magenta: "#bb9af7",
+      red: "#f7768e",
+      yellow: "#e0af68",
+      cyan: "#7dcfff",
+      white: "#c0caf5",
+      black: "#0f0f14",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
