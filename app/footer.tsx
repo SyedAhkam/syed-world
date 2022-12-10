05a885a6f5c8a3c72cba42f8340007cc275d49a9
Syed Ahkam <smahkam57@gmail.com>
2022-12-10 22:59:22 +0530

    Popper js based footer menu

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import Link from "next/link";
+import PopoverDestinationMenu from "../components/footer/PopoverDestinationMenu";
+import useMediaQuery from "../hooks/useMediaQuery";
 
 export default function Footer() {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
   const destinations = [
     { name: "Home", path: "/", isActive: true },
-    { name: "Posts", path: "/posts" },
-    { name: "About Me", path: "/about" },
-    { name: "Contact", path: "/contact" },
+    { name: "Posts", path: "/posts", isActive: false },
+    { name: "About Me", path: "/about", isActive: false },
+    { name: "Contact", path: "/contact", isActive: false },
   ];
 
   const socials = [
@@ -14,6 +20,27 @@ export default function Footer() {
     { name: "", path: "https://linkedin.com/in/syedahkam/" },
     { name: "ﭮ", path: "#coming-soon" },
   ];
+
+  if (!isDesktop) {
+    return (
+      <footer className="fixed bottom-2 flex w-screen flex-row items-stretch justify-between justify-items-stretch pr-8">
+        <PopoverDestinationMenu destinations={destinations} />
+
+        {/* TODO: build socials menu responsive */}
+        <div className="flex flex-row space-x-3 px-8 text-left text-4xl text-blue">
+          {socials.map((social) => (
+            <Link
+              className="hover:text-green"
+              href={social.path}
+              key={social.name}
+            >
+              {social.name}
+            </Link>
+          ))}
+        </div>
+      </footer>
+    );
+  }
 
   return (
     <footer className="fixed bottom-8 mx-16 flex w-screen flex-row items-stretch justify-between justify-items-stretch pr-16">
