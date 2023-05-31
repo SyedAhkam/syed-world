101ca5a314757b34fd0fd11cd0d0e2ce0564ad14
Syed Ahkam <smahkam57@gmail.com>
2023-06-01 03:00:48 +0530

    feat: multiple types of stdout messages

--- src/components/common/Stdout.tsx
+++ src/components/common/Stdout.tsx
@@ -10,16 +10,58 @@ export default function Stdout() {
   const page = getPageFromPath(pathName);
 
   const messages = [
-    "Initializing Page renderer: v0.1",
-    `Loaded page: ${page}`,
-    "Rendering requested page",
+    {
+      text: "Site under construction",
+      type: "warning",
+    },
+    {
+      text: "Initializing Page renderer: v0.1",
+      type: "info",
+    },
+    {
+      text: `Loaded page: ${page}`,
+      type: "info",
+    },
+    {
+      text: "Rendering requested page",
+      type: "info",
+    },
   ];
+
+  const getColorByType = (type: string) => {
+    switch (type) {
+      case "info":
+        return "text-blue";
+      case "warning":
+        return "text-yellow";
+      case "error":
+        return "text-red";
+      default:
+        return "text-white";
+    }
+  };
+
+  const getTagByType = (type: string) => {
+    switch (type) {
+      case "info":
+        return "[*]";
+      case "warning":
+        return "[!]";
+      case "error":
+        return "[X]";
+      default:
+        return "[*]";
+    }
+  };
 
   return (
     <div className="hidden flex-col md:flex md:text-xl">
       {messages.map((message, index) => (
         <p key={index}>
-          <span className="text-red">[*]</span> {message}
+          <span className={getColorByType(message.type)}>
+            {getTagByType(message.type)}
+          </span>{" "}
+          {message.text}
           {index === messages.length - 1 && (
             <span className="animate-pulse">...</span>
           )}
