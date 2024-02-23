d1d89d7c240e2ba8e8463268b1605fa27549c135
SyedAhkam <smahkam57@gmail.com>
2024-02-24 01:35:54 +0530

    feat: add email social icon

--- src/components/common/Socials.tsx
+++ src/components/common/Socials.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
-import { FaDiscord, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaDiscord, FaEnvelope, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function Socials() {
   const socials = [
+    { icon: FaEnvelope, path: "mailto:smahkam57@gmail.com" },
     { icon: FaTwitter, path: "https://twitter.com/SyedAhkam1" },
     { icon: FaGithub, path: "https://github.com/SyedAhkam" },
     { icon: FaLinkedin, path: "https://linkedin.com/in/syedahkam/" },
