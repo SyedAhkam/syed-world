23f3d521081ff324f9b8bfc68b3f0d23611d6804
Syed Ahkam <smahkam57@gmail.com>
2024-07-23 22:11:20 +0530

    fix: fixed footer at the bottom

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -3,7 +3,7 @@ import Socials from "@/components/common/Socials";
 
 export default function Footer() {
   return (
-    <footer className="my-8 flex w-full flex-row justify-between px-8 md:px-16">
+    <footer className="my-8 flex w-full flex-row justify-between px-8 md:px-16 fixed bottom-0 bg-blue/10 py-1 backdrop-blur-sm">
       <NavLinks />
       <Socials />
     </footer>
